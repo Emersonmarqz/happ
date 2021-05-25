65606dc2d834bdc5df179f10a15b0e0fb5615dcf
Ricardo Vinicius <ricardo.pgm.2018.2@gmail.com>
2021-05-24 22:53:16 -0300

    Quiz

---
+++ assets/js/questionquiz.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Se todo mundo mundo com quem trabalho fosse como eu, seria mais fácil de deixar as coisas prontas.",
+    answer: "False",
+    options: [
+      "Talvez",
+      "Verdade",
+      "Falso",
+      "Parcialmente Falso"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Os grupos de trabalho deveriam garantir que estão dando o devido crédito por suas contribuições invividuais.",
+    answer: "Verdade",
+    options: [
+      "Talvez",
+      "Verdade",
+      "Falso",
+      "Parcialmente Falso"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Eu sempre me desculpo quando faço algo errado.",
+    answer: "Verdade",
+    options: [
+      "Talvez",
+      "Verdade",
+      "Falso",
+      "Parcialmente Falso"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual dessas se encaixa como soft Skills?",
+    answer: "Aconselhamento",
+    options: [
+      "Análise",
+      "Monitoramento",
+      "Orçamentação",
+      "Aconselhamento"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Integridade é essencial em todas as funções, mas em qual das a seguir é mais crucial?",
+    answer: "Liderança",
+    options: [
+      "Liderança",
+      "Gerenciamento",
+      "Supervisão",
+      "Direção"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ view/quiz.php
@@ -0,0 +1,86 @@
+<!-- Created By CodingNepal - www.codingnepalweb.com  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Awesome Quiz App | CodingNepal</title>
+    <link rel="stylesheet" href="../assets/css/quiz.css">
+    <!-- FontAweome CDN Link for Icons-->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
+    <!-- start Quiz button -->
+    <div class="start_btn"><button>Jogar</button></div>
+
+    <!-- Info Box -->
+    <div class="info_box">
+        <div class="info-title"><span>Regras do Quiz</span></div>
+        <div class="info-list">
+            <div class="info">1. Você terá apenas <span>15 segundos</span> por questão.</div>
+            <div class="info">2. Assim que você seleciona uma opção não poderá selecionar outra.</div>
+            <div class="info">3. Quando o tempo acabar, você não poderá escolher nenhuma opção.</div>
+            <div class="info">4. Você não poderá sair do Quiz quando uma vez iniciado.</div>
+            <div class="info">5. Sua pontuação será baseada no número de respostas corretas.</div>
+        </div>
+        <div class="buttons">
+            <button class="quit">Sair</button>
+            <button class="restart">Continuar</button>
+        </div>
+    </div>
+
+    <!-- Quiz Box -->
+    <div class="quiz_box">
+        <header>
+            <div class="title">Quiz Happen</div>
+            <div class="timer">
+                <div class="time_left_txt">Tempo</div>
+                <div class="timer_sec">15</div>
+            </div>
+            <div class="time_line"></div>
+        </header>
+        <section>
+            <div class="que_text">
+                <!-- Here I've inserted question from JavaScript -->
+            </div>
+            <div class="option_list">
+                <!-- Here I've inserted options from JavaScript -->
+            </div>
+        </section>
+
+        <!-- footer of Quiz Box -->
+        <footer>
+            <div class="total_que">
+                <!-- Here I've inserted Question Count Number from JavaScript -->
+            </div>
+            <button class="next_btn">Próxima</button>
+        </footer>
+    </div>
+
+    <!-- Result Box -->
+    <div class="result_box">
+        <div class="icon">
+            <img src="../assets/img/happen_grey.png" style=" max-width:118px; 
+                    max-height:60px"></a>
+        </div>
+        <div class="complete_text">Você Finalizou o quiz!</div>
+        <div class="score_text">
+            <!-- Here I've inserted Score Result from JavaScript -->
+        </div>
+        <div class="buttons">
+            <button class="restart" id="restart">Jogar Novamente</button>
+            <button class="quit" id="quit">Sair do Quiz</button>
+        </div>
+    </div>
+
+    <!-- Inside this JavaScript file I've inserted Questions and Options only -->
+    <script src="../assets/js/questionquiz.js"></script>
+
+    <!-- Inside this JavaScript file I've coded all Quiz Codes -->
+    <script src="../assets/js/quiz.js"></script>
+
+</body>
+
+</html>
